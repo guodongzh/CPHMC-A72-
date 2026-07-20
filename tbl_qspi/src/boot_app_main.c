df72b50f017a68e923b674d194de53b380182590
liurui <1435996384@qq.com>
2026-07-20 13:37:03 +0800

    refactor(tbl_qspi): 替换SVN版本为VSC版本并更新工具路径
    1.  更新TOOL_PATH为相对路径../../CPHMC_1A/tools
    2.  将代码中的APP_SVN_VERSION替换为APP_VSC_VERSION，打印格式改为16进制
    3.  更新prebuild脚本路径，删除update_svn_version.mk文件
    4.  补全.cproject中编译选项的名称描述

--- tbl_qspi/src/boot_app_main.c
+++ tbl_qspi/src/boot_app_main.c
@@ -92,12 +92,12 @@ int32_t main(void)
     UART_stdioInit2(BOARD_UART_INSTANCE, &params);
     putchar_ = UART_putc;
 
-    UART_printf("\nMCU R5F Boot App build at %d-%d-%d - %s, APP SVN VER = %d, started at %d usecs\n",
+    UART_printf("\nMCU R5F Boot App build at %d-%d-%d - %s, APP VSC VER = 0x%08x, started at %d usecs\n",
                 APP_BUILD_YEAR,
                 APP_BUILD_MONTH,
                 APP_BUILD_DAY,
                 __TIME__,
-                APP_SVN_VERSION,
+                APP_VSC_VERSION,
                 BootApp_GetTimeInMicroSec(CSL_armR5PmuReadCntr(CSL_ARM_R5_PMU_CYCLE_COUNTER_NUM)));
 
     ret = BootApp_SetupSciServer();
